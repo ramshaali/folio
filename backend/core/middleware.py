af6975bf98d9700836e1b9f598e4ad26130e5917
zyenak <158252686+zyena-kamran@users.noreply.github.com>
2025-11-10 00:41:19 +0500

    save middleware

--- backend/core/middleware.py
+++ backend/core/middleware.py
@@ -1,4 +1,5 @@
 from fastapi import Request, HTTPException
+from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 from dotenv import load_dotenv
 import os
@@ -9,14 +10,17 @@ load_dotenv()
 API_KEY = os.environ.get("APP_API_KEY")
 
 async def api_key_validator(request: Request, call_next):
-    # Allow specific routes without API key
-    if request.url.path.startswith("/api/auth/status"):
+    if request.method == "OPTIONS":
         return await call_next(request)
+
 
     # Check the API key header
     client_key = request.headers.get("x-api-key")
 
     if client_key != API_KEY:
-        raise HTTPException(status_code=401, detail="Invalid or missing API key")
+        return JSONResponse(
+            status_code=401,
+            content={"error": "Invalid or missing APP API key"}
+        )
 
     return await call_next(request)
--- backend/core/router_register.py
+++ backend/core/router_register.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-from routers import generate, session, auth
+from routers import generate, session
 
 def register_routers(app: FastAPI):
     app.include_router(session.router, prefix="/api/session", tags=["Session"])
     app.include_router(generate.router, prefix="/api/generate", tags=["Generation"])
-    app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
