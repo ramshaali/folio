7ff93440bb787c1d2094e9509e5cc6b2059f1b08
zyenak <158252686+zyena-kamran@users.noreply.github.com>
2025-11-09 23:55:25 +0500

    vite prod configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,5 +14,15 @@ export default defineConfig(({ mode }) => {
     define: {
       'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL),
     },
+    server: {
+      host: true, // or '0.0.0.0'
+      port: 8080, // Optional: set a fixed port
+      strictPort: true, // Optional: fail if port is unavailable
+    },
+    preview: {
+      host: true, // Also needed for 'npm run preview'
+      port: 8080,
+      strictPort: true,
+    }
   }
 })
